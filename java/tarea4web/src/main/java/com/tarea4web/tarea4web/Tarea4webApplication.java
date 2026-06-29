02b8ff2484dc03423e03f6381c463c9795b9c9d3
lucas.brule@ug.uchile.cl <lucasbrule01@gmail.com>
2026-06-29 18:41:25 -0400

    Setup Tarea 4

---
+++ java/tarea4web/src/main/java/com/tarea4web/tarea4web/Tarea4webApplication.java
@@ -0,0 +1,13 @@
+package com.tarea4web.tarea4web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea4webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea4webApplication.class, args);
+	}
+
+}
